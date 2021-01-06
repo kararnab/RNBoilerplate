4d436c2258341cc86dad37699eae878f8cd299c2
Arnab Kar <30458810+kararnab@users.noreply.github.com>
2021-01-06 22:13:17 +0530

    Implemented logout logic

--- app/api/MyService.tsx
+++ app/api/MyService.tsx
@@ -1,18 +1,31 @@
 import axios from 'axios'
 import { clearAll } from '../redux/action/action1';
-import { Constants } from '../util/constants'
+import { Constants } from '../util/constants';
+import { store } from "../App";
 
 var client =  axios.create({
   baseURL: Constants.getBaseUrl(),
 })
 
+const BaseHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
+
+//Setting JWT for next calls
+export const setClientToken = token => {
+  client.interceptors.request.use(function(config) {
+    // config.headers.Authorization = `${token}`;
+    config.headers = {...BaseHeaders, ...{ Authorization: token }}
+    return config;
+  });
+};
+
 client.interceptors.response.use(response => {
   return response;
 }, error => {
  if (error.response.status === 401) {
-  //place your reentry code
-  //TODO:
-  AppContainer.dispatch(clearAll())
+  store.dispatch(clearAll())
  }
  return error;
 });
